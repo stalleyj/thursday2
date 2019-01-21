81ebc988f46b30f75e8ff027ac85f93d0fca3792
Julie Stalley <julie_stalley@uk.ibm.com>
2019-01-21 15:07:52 +0000

    test

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ require('./routers/index')(app, server);
 
 const port = process.env.PORT || localConfig.port;
 server.listen(port, function(){
-    logger.info(`Another message to check`);
+    logger.info(`Another message to check 2`);
   logger.info(`julie NOT listening on http://localhost:${port}/appmetrics-dash`);
   logger.info(`toby is not listening on http://localhost:${port}`);
 });
